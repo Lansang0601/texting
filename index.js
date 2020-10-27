3c36dc8e17ddd97e7e5946ed74ce06592b1c54bc
lansang0905 <lansang060189@naver.com>
2020-10-28 07:58:33 +0900

    modify token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.token;
+const token = process.argv.length == 2 ? process.env.token : "NzQ3NDE0OTM0MzIwNTc4NjYx.X0OiZg.YLO66GjM7usU1OpXVIjjINUaxms";
 const moment = require("moment");
 require("moment-duration-format");
 const welcomeChannelName = "일반";
